ef25ed7efc43571bd103c4f802da5b52fdfb9394
Giuliano Losa <giuliano@losa.fr>
2016-01-08 09:53:51 -0500

    Finished the composition proof with the GLB init rule.

--- Isabelle2/Composition.thy
+++ Isabelle2/Composition.thy
@@ -83,6 +83,11 @@ definition inv5 where
         \<and> s = \<Sqinter>S \<star> cs \<and> set cs \<subseteq> propCmd r2"
 declare inv5_def[inv_defs]
 
+definition inv6 where
+  "inv6 r \<equiv> let r1 = fst r; r2 = snd r in
+    finite (propCmd r1) \<and> finite (propCmd r2)"
+declare inv6_def[inv_defs]
+
 definition inv8 where
   "inv8 r \<equiv> let r1 = fst r; r2 = snd r in
     \<forall> s \<in> cgc_state.toNext r1 . 
@@ -172,6 +177,10 @@ lemma inv3:"invariant composition inv3"
   by try_solve_inv
 declare inv3[invs]
 
+lemma inv6:"invariant composition inv6"
+  by try_solve_inv
+declare inv6[invs]
+
 lemma inv4:"invariant composition inv4"
   apply (try_solve_inv mydefs:non_trivial_def less_eq_def)
   apply (metis subset_insertI2)
@@ -206,8 +215,24 @@ lemma inv9:"invariant composition inv9"
   apply (try_solve_inv mydefs:non_trivial_def less_eq_def)
   apply (metis subset_insertI2)
   apply metis
-  
-  
+  subgoal premises prems for s t a x5
+  proof -
+    have 1:"\<And> S s1 . \<lbrakk>S \<subseteq> insert x5 (cgc_state.toNext (fst s)); s1 \<in> S\<rbrakk> 
+      \<Longrightarrow> \<exists>cs. s1 = \<bottom> \<star> cs \<and> set cs \<subseteq> propCmd (fst s)" using prems(12) by auto
+    have 2:"finite (cgc_state.toNext (fst s))" using prems by auto
+    show ?thesis 
+    proof clarify
+      fix S x
+      assume a1:"S \<noteq> {}" and a2:"S \<subseteq> insert x5 (cgc_state.toNext (fst s))"
+      hence 4:"\<And> s1 . s1 \<in> S \<Longrightarrow> \<exists>cs. s1 = \<bottom> \<star> cs \<and> set cs \<subseteq> propCmd (fst s)" 
+        using 1 by auto
+      have 5:"finite S" using 2 a2 by (metis finite_insert infinite_super) 
+      from 4 5 a1 glb_common_set[of "S" "propCmd (fst s)"] 
+        show "\<exists>cs. \<Sqinter> S = \<bottom> \<star> cs \<and> set cs \<subseteq> propCmd (fst s)" by fast
+    qed
+  qed
+done
+declare inv9[invs]
   
 
 definition refmap where
@@ -240,8 +265,42 @@ apply(simp add:is_ref_map_def refmap_def, rule conjI; clarify)
    apply (split_conjs?)
    apply (metis Un_upper1 subset_trans)
    apply (smt ComposableGC.l3 ComposableGC_axioms Un_iff antimono compat_sym l1 pre_CStruct.trans subset_empty)
-   apply (smt ComposableGC.l3 ComposableGC_axioms coboundedI1 less_eq_def pre_CStruct.trans subset subset_empty)
-   apply (simp add:fun_eq_iff)
+   subgoal
+  proof -
+    fix a :: "('a, 'b, 'c) cgc_state" and b :: "('a, 'b, 'c) cgc_state" and aa :: "('a, 'b, 'c) cgc_state" and ba :: "('a, 'b, 'c) cgc_state" and x31 :: 'a and x32 :: 'c
+    assume a1: "(\<exists>S. S \<noteq> {} \<and> S \<subseteq> {\<bottom>} \<and> (\<exists>cs. set cs \<subseteq> propCmd a \<and> x31 = \<Sqinter> S \<star> cs)) \<and> x32 \<in> learners \<and> (\<forall>l\<in>learners. \<forall>s'\<in>learned a l. compat2 s' x31) \<and> (\<forall>x\<in>cgc_state.toNext a. x31 \<preceq> x) \<and> aa = a \<lparr>learned := (learned a) (x32 := insert x31 (learned a x32))\<rparr> \<and> b = ba"
+    assume a2: "finite (cgc_state.toNext a) \<and> finite (cgc_state.toNext ba)"
+    assume a3: "\<forall>s\<in>cgc_state.toNext ba. \<exists>S. S \<noteq> {} \<and> S \<subseteq> cgc_state.toNext a \<and> (\<exists>cs. s = \<Sqinter> S \<star> cs \<and> set cs \<subseteq> propCmd ba)"
+    { fix aaa :: 'a
+      obtain AA :: "'a \<Rightarrow> 'a set" and bbs :: "'a \<Rightarrow> 'b list" where
+        ff1: "\<forall>aa. aa \<notin> cgc_state.toNext ba \<or> AA aa \<noteq> {} \<and> AA aa \<subseteq> cgc_state.toNext a \<and> aa = \<Sqinter> AA aa \<star> bbs aa \<and> set (bbs aa) \<subseteq> propCmd ba"
+        using a3 by metis
+      { assume "aaa \<in> cgc_state.toNext ba"
+        { assume "cgc_state.toNext a \<noteq> {}"
+          moreover
+          { assume "cgc_state.toNext a \<noteq> {} \<and> aaa \<in> cgc_state.toNext ba"
+            moreover
+            { assume "AA aaa \<noteq> {} \<and> \<not> x31 \<preceq> aaa \<and> cgc_state.toNext a \<noteq> {}"
+              moreover
+              { assume "\<exists>aa. \<not> aa \<preceq> aaa \<and> AA aaa \<noteq> {} \<and> aa \<preceq> \<Sqinter> cgc_state.toNext a"
+                then have "(\<exists>a. \<not> a \<preceq> aaa \<and> a \<preceq> \<Sqinter> AA aaa) \<or> {} \<noteq> AA aaa \<and> \<not> \<Sqinter> cgc_state.toNext a \<preceq> \<Sqinter> AA aaa \<or> aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+                  by (metis local.trans)
+                then have "AA aaa \<subseteq> cgc_state.toNext a \<longrightarrow> aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+                  using ff1 a2 by (metis antimono less_eq_def local.trans) }
+              ultimately have "AA aaa \<subseteq> cgc_state.toNext a \<longrightarrow> aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+                using a2 a1 by (metis l3) }
+            ultimately have "AA aaa \<subseteq> cgc_state.toNext a \<longrightarrow> aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+              using ff1 by metis }
+          ultimately have "AA aaa \<subseteq> cgc_state.toNext a \<longrightarrow> aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+            by metis }
+        then have "aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+          using ff1 by (metis empty_iff subsetI subset_antisym) }
+      then have "aaa \<notin> cgc_state.toNext ba \<or> x31 \<preceq> aaa"
+        by metis }
+    then show "\<forall>a\<in>cgc_state.toNext ba. x31 \<preceq> a"
+      by metis
+  qed
+  apply (simp add:fun_eq_iff)
 
    apply(rule_tac x="refmap (a,b)" in exI)
    apply(rule_tac x="[(Learn2 x41 x42,refmap (aa,ba))]" in exI)
@@ -249,9 +308,6 @@ apply(simp add:is_ref_map_def refmap_def, rule conjI; clarify)
    apply (split_conjs?)
    defer
    apply (metis (mono_tags, lifting) ComposableGC.l3 ComposableGC_axioms Un_iff antimono l1 pre_CStruct.trans)
-   apply (simp add:fun_eq_iff)
-   
-   sorry
    apply (simp add:fun_eq_iff)
 
    apply(rule_tac x="refmap (a,b)" in exI)
@@ -262,8 +318,43 @@ apply(simp add:is_ref_map_def refmap_def, rule conjI; clarify)
    apply (rule_tac x="[(ToNext x6,refmap (aa,ba))]" in exI)
    apply (simp add:ref_defs spec_def actions_defs cgc_ioa_defs actions_defs inv_defs refmap_def non_trivial_def)
    apply (split_conjs?)
-   apply (metis sup.mono l2)
-   apply (metis Un_iff less_eq_def pre_CStruct.trans)
+   subgoal
+  proof -
+    fix a :: "('a, 'b, 'c) cgc_state" and b :: "('a, 'b, 'c) cgc_state" and aa :: "('a, 'b, 'c) cgc_state" and ba :: "('a, 'b, 'c) cgc_state" and x6 :: 'a
+    assume a1: "\<forall>S. S \<noteq> {} \<and> S \<subseteq> cgc_state.toNext aa \<longrightarrow> (\<exists>cs. \<Sqinter> S = \<bottom> \<star> cs \<and> set cs \<subseteq> propCmd aa)"
+    assume a2: "\<exists>S. S \<noteq> {} \<and> S \<subseteq> cgc_state.toNext aa \<and> (\<exists>cs. x6 = \<Sqinter> S \<star> cs \<and> set cs \<subseteq> propCmd b)"
+    obtain bbs :: "'a set \<Rightarrow> 'b list" where
+      f3: "\<forall>A. (A = {} \<or> \<not> A \<subseteq> cgc_state.toNext aa) \<or> \<Sqinter> A = \<bottom> \<star> bbs A \<and> set (bbs A) \<subseteq> propCmd aa"
+      using a1 by metis
+    obtain AA :: "'a set" and bbsa :: "'b list" where
+      f4: "AA \<noteq> {} \<and> AA \<subseteq> cgc_state.toNext aa \<and> x6 = \<Sqinter> AA \<star> bbsa \<and> set bbsa \<subseteq> propCmd b"
+      using a2 by moura
+    then have "set (bbs AA) \<union> set bbsa \<subseteq> propCmd aa \<union> propCmd b"
+      using f3 by (metis sup.mono)
+    then have "set (append bbsa (bbs AA)) \<subseteq> propCmd b \<union> propCmd aa"
+      by (metis Un_commute set_append)
+    then show "\<exists>A. A \<noteq> {} \<and> A \<subseteq> {\<bottom>} \<and> (\<exists>bs. set bs \<subseteq> propCmd aa \<union> propCmd b \<and> x6 = \<Sqinter> A \<star> bs)"
+      using f4 f3 by (metis Un_commute empty_not_insert pre_CStruct.exec_append singleton subsetI)
+  qed
+  apply (metis UnE Un_absorb1 finite_Un less_eq_def local.boundedI local.trans subset_eq)
+  subgoal
+  proof -
+    fix a :: "('a, 'b, 'c) cgc_state" and b :: "('a, 'b, 'c) cgc_state" and aa :: "('a, 'b, 'c) cgc_state" and ba :: "('a, 'b, 'c) cgc_state" and x41 :: 'a and x42 :: 'c
+    assume a1: "a = aa \<and> (\<exists>S. S \<noteq> {} \<and> S \<subseteq> cgc_state.toNext aa \<and> (\<exists>cs. set cs \<subseteq> propCmd b \<and> x41 = \<Sqinter> S \<star> cs)) \<and> x42 \<in> learners \<and> (\<forall>l\<in>learners. \<forall>s'\<in>learned b l. compat2 s' x41) \<and> (\<forall>x\<in>cgc_state.toNext b. x41 \<preceq> x) \<and> cgc_state.toNext aa \<noteq> {} \<and> ba = b \<lparr>learned := (learned b)(x42 := insert x41 (learned b x42))\<rparr>"
+    assume a2: "\<forall>S. S \<noteq> {} \<and> S \<subseteq> cgc_state.toNext aa \<longrightarrow> (\<exists>cs. \<Sqinter> S = \<bottom> \<star> cs \<and> set cs \<subseteq> propCmd aa)"
+    obtain AA :: "'a set" and bbs :: "'b list" where
+      f3: "a = aa \<and> (AA \<noteq> {} \<and> AA \<subseteq> cgc_state.toNext aa \<and> set bbs \<subseteq> propCmd b \<and> x41 = \<Sqinter> AA \<star> bbs) \<and> x42 \<in> learners \<and> (\<forall>c. c \<notin> learners \<or> (\<forall>a. a \<notin> learned b c \<or> compat2 a x41)) \<and> (\<forall>a. a \<notin> cgc_state.toNext b \<or> x41 \<preceq> a) \<and> cgc_state.toNext aa \<noteq> {} \<and> ba = b \<lparr>learned := (learned b)(x42 := insert x41 (learned b x42))\<rparr>"
+      using a1 by auto
+    obtain bbsa :: "'a set \<Rightarrow> 'b list" where
+      f4: "\<forall>A. (A = {} \<or> \<not> A \<subseteq> cgc_state.toNext aa) \<or> \<Sqinter> A = \<bottom> \<star> bbsa A \<and> set (bbsa A) \<subseteq> propCmd aa"
+      using a2 by metis
+    then have "set (bbsa AA) \<union> set bbs \<subseteq> propCmd aa \<union> propCmd b"
+      using f3 by (metis sup.mono)
+    then have "set (append bbs (bbsa AA)) \<subseteq> propCmd b \<union> propCmd aa"
+      by (metis Un_commute set_append)
+    then show "\<exists>A. A \<noteq> {} \<and> A \<subseteq> {\<bottom>} \<and> (\<exists>bs. set bs \<subseteq> propCmd aa \<union> propCmd b \<and> x41 = \<Sqinter> A \<star> bs)"
+      using f4 f3 by (metis Un_commute empty_not_insert pre_CStruct.exec_append singleton subsetI)
+  qed
 done
 
 lemma trace_incl:"traces composition \<subseteq> traces spec"
