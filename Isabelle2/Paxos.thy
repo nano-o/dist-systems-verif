86415e66cdb3b8070221c091e4fc2e6150ca50c0
Giuliano Losa <giuliano@losa.fr>
2016-02-08 14:04:07 -0500

    Working on Paxos.thy

--- Isabelle2/Paxos.thy
+++ Isabelle2/Paxos.thy
@@ -98,12 +98,12 @@ definition higher where
   
 definition choosable where
   "choosable r v b \<equiv>
-    (\<exists> q \<in> quorums . (\<forall> a \<in> q . (case (ballot r) a of 
-          None \<Rightarrow> True 
+    (\<exists> q \<in> quorums . (\<forall> a \<in> q . (case (ballot r) a of
+          None \<Rightarrow> True
         | Some ba \<Rightarrow> (ba > b \<longrightarrow> (vote r) a b = Some v))))"
 
 definition safe_at where
-  "safe_at r v b \<equiv> 
+  "safe_at r v b \<equiv>
     (\<forall> b2 . \<forall> v2 .
       ((b2 < b \<and> choosable r v2 b2) \<longrightarrow> (v = v2)))"
 
@@ -114,18 +114,114 @@ definition well_formed where
   "well_formed r \<equiv> \<forall> a \<in> acceptors . \<forall> b . 
     (case (ballot r) a of None \<Rightarrow> True | Some ab \<Rightarrow> ab < b) \<longrightarrow> (vote r) a b = None"
 
-lemma "v \<in> vals \<Longrightarrow> safe_at r v (bot::'b::wellorder_bot)"
+lemma "safe_at r v (bot::'b::wellorder_bot)"
 by (auto simp add:safe_at_def)
 
 lemma
   assumes "chosen_at r v (b::nat)"
   shows "choosable r v b" using assms
-  by (auto simp add:chosen_at_def choosable_def) (smt option.case_eq_if) 
+  by (auto simp add:chosen_at_def choosable_def) (smt option.case_eq_if)
 
 lemma safe_at_prec:
   assumes "safe_at r v (b::nat)" and "b2 < b"
   shows "safe_at r v b2"
   using assms by (meson order.strict_trans safe_at_def)
+
+lemma quorum_vote:
+  assumes "q1 \<in> quorums" and "q2 \<in> quorums"
+  and "\<And> a . a \<in> q1 \<Longrightarrow> vote r a b = Some v1"
+  and "\<And> a . a \<in> q2 \<Longrightarrow> vote r a b = Some v2"
+  shows "v1 = v2" 
+proof -
+  obtain a where "a \<in> q1" and "a \<in> q2"
+  proof -
+    have "q1 \<inter> q2 \<noteq> {}" using assms(1,2)
+      by (metis Paxos_axioms Paxos_def)
+    hence "\<exists> a . a \<in> q1 \<and> a \<in> q2"
+      by (metis disjoint_iff_not_equal) 
+    with that show ?thesis by auto
+  qed
+  thus ?thesis using assms(3,4) by force
+qed   
+
+lemma chosen_at_same:
+  assumes "chosen_at r v1 b1" and "chosen_at r v2 b1"
+  shows "v1 = v2" using quorum_vote assms
+  by (auto simp add:chosen_at_def) fast
+
+lemma 
+  assumes "\<And> (v::'v) . choosable r v b"
+  and "safe_at r v (Suc b)" and "(v1::'v) \<noteq> v2"
+  shows False using assms 
+  by (auto simp add:safe_at_def)(metis lessI) 
+
+lemma
+  assumes "safe_at (r::('v, 'acc, nat)p_state) v b" and "(v1::'v) \<noteq> v2" and "b \<noteq> 0"
+  shows "\<exists> q \<in> quorums . \<forall> a \<in> q . case ballot r a of None \<Rightarrow> False | Some b2 \<Rightarrow> b2 \<ge> b"
+  using assms
+  proof(induct b)
+    case 0 thus ?case by auto
+  next
+    case (Suc b)
+    assume "b \<noteq> 0"
+    obtain q where "q \<in> quorums" and "\<And> a . a \<in> q \<Longrightarrow> case ballot r a of None \<Rightarrow> False | Some b2 \<Rightarrow> b2 \<ge> b"
+      using Suc.hyps Suc.prems(2,3)
+    by auto (metis (full_types) Suc.hyps Suc.prems(1) \<open>b \<noteq> 0\<close> lessI safe_at_prec)
+    assume "case ballot r a of None \<Rightarrow> False | Some b2 \<Rightarrow> b2 = b" if "a \<in> q" for a
+    hence "choosable r v b" for v
+      by (smt Suc_leI Suc_n_not_le_n \<open>q \<in> quorums\<close> choosable_def option.case_eq_if)
+    hence False using Suc.prems(1)
+      by (auto simp add:safe_at_def) (metis assms(2) lessI)
+    oops
+    
+lemma
+  fixes v b v1 v2 r
+  assumes "safe_at (r::('v, 'acc, nat)p_state) v (Suc b)" and "(v1::'v) \<noteq> v2"
+  obtains q co where
+  "q \<in> quorums" and "\<And> a . a \<in> q \<Longrightarrow> case ballot r a of None \<Rightarrow> False | Some b2 \<Rightarrow> b2 \<ge> Suc b"
+  and "case co of None \<Rightarrow> True | Some c \<Rightarrow> (
+    c \<le> b 
+    \<and> safe_at r v c
+    \<and> (\<forall> a \<in> q . (case vote r a c of None \<Rightarrow> True | Some v2 \<Rightarrow> v2 = v))
+    \<and> (\<forall> a \<in> q . \<forall> d . (d > c \<and> d \<le> b) \<longrightarrow> vote r a d = None))"
+  and "\<And> d a . \<lbrakk>d \<ge> (case co of None \<Rightarrow> (0::nat) | Some c \<Rightarrow> Suc c); d \<le> b; a \<in> q\<rbrakk> \<Longrightarrow> vote r a d = None"
+    nitpick[card 'acc=1, card 'l = 1, card 'v=2, show_all, card "'x option"=50,
+      card nat = 2, show_all]
+
+lemma safe1:
+  assumes "safe (r::('v, 'acc, nat)p_state)"
+  and "chosen_at r v1 b1" and "chosen_at r v2 b2"
+  and "b1 \<le> b2"
+  shows "v1 = v2" using assms
+proof (induct b2) print_cases
+  case 0 
+  from "0.prems"(2-4) show ?case using quorum_vote
+  by (auto simp add:chosen_at_def) fast
+next
+  case (Suc b2)
+  { assume "b1 = Suc b2"
+    hence ?thesis using Suc.hyps Suc.prems chosen_at_same by metis }
+  moreover
+  { assume "b1 \<le> b2"
+    hence ?thesis sorry }
+  ultimately show ?thesis by (metis Suc.prems(4) le_SucE) 
+qed
+
+lemma safe:
+  assumes "safe (r::('v, 'acc, nat)p_state)"
+  and "chosen r v1" and "chosen r v2"
+  shows "v1 = v2" 
+proof -
+  obtain b1 b2 where b1:"chosen_at r v1 b1"
+  and b2:"chosen_at r v2 b2" using assms(2,3) 
+    by (auto simp add:chosen_def)
+  have ?thesis if "b1 \<le> b2" using safe1 assms(1) b1 b2 that by metis
+  moreover
+  have ?thesis if "b2 \<le> b1" using safe1 assms(1) b1 b2 that by metis
+  ultimately show ?thesis by linarith
+qed
+  
+  
   
 (* 
 lemma 
