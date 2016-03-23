565cbf9bc2fdd568fc92e65075f539a3815f8e02
Giuliano Losa <giuliano@losa.fr>
2016-03-23 16:04:33 -0400

    Working on MultiPaxosHist2.thy

--- Isabelle2/MultiPaxosHist2.thy
+++ Isabelle2/MultiPaxosHist2.thy
@@ -5,15 +5,17 @@ begin
 
 text {* A version of MultiPaxos with history variables added *}
 
-text {* TODO: Prove that MultiPaxos refines it, preferrably automatically *}
+text {* TODO: Prove that MultiPaxos refines it, preferably automatically *}
 
 record 'v mph_state =
   mp_state :: "'v mp_state"
   vote_hist :: "inst \<Rightarrow>f acc \<Rightarrow>f bal \<Rightarrow>f 'v cmd option"
 
-definition init_state :: "acc fset \<Rightarrow> 'v mph_state" where
-  "init_state accs \<equiv> \<lparr>
-    mp_state = MultiPaxos3.init_state accs,
+context mp_ioa begin
+
+definition init_state :: "nat \<Rightarrow> 'v mph_state" where
+  "init_state n \<equiv> \<lparr>
+    mp_state = mp_start,
     vote_hist = K$ K$ K$ None \<rparr>"
 
 end
