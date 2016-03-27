ff1aef3bd789077e7f5eb8015ca7eb199722c1c4
Giuliano Losa <giuliano@losa.fr>
2016-03-27 17:50:52 -0400

    Added a cardinality-based definition of quorums to Quorums.thy.

--- Isabelle2/Quorums.thy
+++ Isabelle2/Quorums.thy
@@ -24,6 +24,25 @@ lemma quorums_value_same:
   shows "x = y" using assms
 by (metis quorum_inter_witness) 
 
+definition nas where "nas \<equiv> fcard acceptors"
+
+definition mp_quorums where
+  "mp_quorums \<equiv> ffilter (\<lambda> s . 2 * fcard s > nas) (fPow acceptors)"
+
+lemma assumes "q1 |\<in>| mp_quorums" "q2 |\<in>| mp_quorums"
+shows "q1 |\<inter>| q2 \<noteq> {||}"
+proof -
+  have 1:"2 * fcard q1 > nas" and 2:"q1 |\<subseteq>| acceptors" using assms(1) 
+  by (simp_all add:mp_quorums_def)
+  moreover
+  have 4:"2 * fcard q2 > nas" and 3:"q2 |\<subseteq>| acceptors" using assms(2) 
+  by (simp_all add:mp_quorums_def)
+  have "fcard q1 + fcard q2 > nas"
+  by (smt 1 4 add_le_imp_le_right add_left_mono add_lessD1 leD less_imp_add_positive less_le linorder_neqE_nat mult_2) 
+  with 2 3 show "q1 |\<inter>| q2 \<noteq> {||}"
+  by (metis fcard_funion_disjoint fcard_mono leD le_sup_iff nas_def) 
+qed
+
 end
 
 end
