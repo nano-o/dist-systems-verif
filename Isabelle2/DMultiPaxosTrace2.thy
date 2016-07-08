297c82d8cf3a50f2a5546dc55ddfad02db2d9305
xxan15 <xxan15@vt.edu>
2016-07-08 17:50:56 -0400

    "A new trace-based version"

--- Isabelle2/DMultiPaxosTrace2.thy
+++ Isabelle2/DMultiPaxosTrace2.thy
@@ -1,4 +1,4 @@
-theory DMultiPaxosTrace
+theory DMultiPaxosTrace2
 imports "~~/src/HOL/Library/FinFun_Syntax"
 begin
 
@@ -7,18 +7,111 @@ type_synonym bal = nat
 type_synonym inst = nat
 type_synonym cmd = nat
 
-datatype msgs = 
-  Msg1a bal|
-  Msg1b acc inst bal "bal \<times> cmd option"|
-  Msg2a inst bal cmd
+definition nas where "nas \<equiv> 3"
 
 datatype event = 
-  Propose cmd|
   Phase1a bal|
-  Phase1b acc inst bal "bal \<times> cmd option"|
-  Phase2a inst bal cmd nat|
+  Phase1b acc inst bal "(bal \<times> cmd) option"|
+  Phase2a inst bal cmd |
   VoteCmd acc inst bal cmd
 
+fun started where
+  "started b [] = False"
+| "started b ((Phase1a b2)#es) = (b = b2 \<or> started b es)"
+| "started b (_ # es) = started b es"
+
+fun ballot_2 where
+  "ballot_2 a [] = 0"
+| "ballot_2 a (e#es) = (let b = (ballot_2 a es) in
+    case e of Phase1b a2 _ b2 _ \<Rightarrow> (if a = a2 then b2 else b) | _ \<Rightarrow> b)"
+
+fun last_vote_2 where
+  "last_vote_2 a i [] = None"
+| "last_vote_2 a i (e#es) = (case e of VoteCmd a2 i2 b c \<Rightarrow> 
+    (if (a = a2 \<and> i = i2) then Some (b,c) else last_vote_2 a i es)
+    | _ \<Rightarrow> last_vote_2 a i es)"
+
+definition is_quorum where "is_quorum s \<equiv> 2 * (card s) > nas"
+
+(*fun safe_value_rec where
+  "safe_value_rec _ b c [] seen m = 
+    (if is_quorum seen then (case m of None \<Rightarrow> True | Some (b2,c2) \<Rightarrow> c = c2) else False)"
+| "safe_value_rec i b c ((VoteCmd a i2 b2 c2)#es) seen m =
+    (if (a \<notin> seen \<and> i2 = i) then
+      let seen' = (seen \<union> {a});
+          m' = (if b2 < b then (case m of None \<Rightarrow> Some (b2,c2) | Some (b3,c3) \<Rightarrow> if b2 > b3 
+            then Some (b2,c2) else m) else m)
+      in if is_quorum seen' 
+        then (case m' of None \<Rightarrow> True | Some (b3,c3) \<Rightarrow> c = c3)
+        else safe_value_rec i b c es seen' m' 
+    else safe_value_rec i b c es seen m)"
+| "safe_value_rec i b c (_#es) seen m = safe_value_rec i b c es seen m"*)
+fun safe_value_rec where
+  "safe_value_rec _ b c [] seen m = 
+    (if is_quorum seen then (case m of None \<Rightarrow> True | Some (b2,c2) \<Rightarrow> c = c2) else False)"
+| "safe_value_rec i b c ((Phase1b a i2 b2 vs)#es) seen m =
+    (if (a \<notin> seen \<and> i2 = i) then
+      let seen' = (seen \<union> {a}); c2 = map_option snd vs;
+          m' = (if b2 \<le> b then (case m of None \<Rightarrow> Some (b2,c2) | Some (b3,c3) \<Rightarrow> if b2 > b3 
+            then Some (b2,c2) else m) else m)
+      in if is_quorum seen' 
+        then (case m' of None \<Rightarrow> True | Some (b3,c3) \<Rightarrow> c = c3)
+        else safe_value_rec i b c es seen' m' 
+    else safe_value_rec i b c es seen m)"
+| "safe_value_rec i b c (_#es) seen m = safe_value_rec i b c es seen m"
+
+definition safe_value where 
+  "safe_value i b c es = safe_value_rec i b c es {} None"
+
+lemma assumes "b > 0" shows "\<not> safe_value i b c es" quickcheck oops
+
+fun suggestion where 
+  "suggestion i b [] = None"
+| "suggestion i b ((Phase2a i2 b2 c)#es) = (if i2 = i \<and> b2 = b then Some c else suggestion i b es)"
+| "suggestion i b (_#es) = suggestion i b es"
+
+fun decide_rec where
+  "decide_rec i b c [] seen = False"
+| "decide_rec i b c ((VoteCmd a i2 b2 c2)#es) seen = (if (i = i2 \<and> b = b2 \<and> a \<notin> seen) then
+    let seen' = seen \<union> {a} 
+    in if (is_quorum seen') 
+      then c2 = c
+      else decide_rec i b c es seen'
+  else decide_rec i b c es seen)"
+| "decide_rec i b c (_#es) seen = decide_rec i b c es seen"
+
+definition decide where "decide i b c es \<equiv> decide_rec i b c es {}"
+
+fun paxos where
+  "paxos [] = True"
+| "paxos ((Phase1a b) # es) = (b > 0 \<and> paxos es)"
+| "paxos ((Phase1b a i b r) # es) = (started b es \<and> ballot_2 a es \<le> b \<and> r = last_vote_2 a i es \<and> paxos es)"
+| "paxos ((Phase2a i b c) # es) = (safe_value i b (Some c) es \<and> paxos es)"
+| "paxos ((VoteCmd a i b c) # es) = (suggestion i b es = Some c \<and> paxos es)"
+
+lemma "\<lbrakk>paxos es; ballot_2 a es = b\<rbrakk> \<Longrightarrow> (started b es \<or> b = 0)" oops
+
+lemma assumes "paxos es" and "a \<noteq> a2" 
+  shows "last_vote_2 a i es = None \<or> last_vote_2 a2 i es = None" quickcheck oops
+
+lemma assumes "paxos es" shows "\<not> started b es" nitpick oops
+
+lemma assumes "paxos es" 
+  shows "\<not> (started b es \<and> ballot_2 a es \<le> 1 \<and> r = last_vote_2 a i es \<and> r \<noteq> None)" quickcheck[eval="ballot_2 a es"]
+oops
+
+lemma assumes "paxos es" and "b \<noteq> 0" shows "suggestion i b es = None" nitpick
+oops 
+
+lemma assumes "paxos (e#es)"
+  shows "\<not> (e = Phase1b a i b r)" quickcheck oops
+
+lemma assumes "paxos es" and "a \<noteq> a2"
+  shows "last_vote_2 a i es = None \<or> last_vote_2 a2 i es = None" quickcheck
+
+
+lemma assumes "paxos es" and "decide i b c es" and "decide i b2 c2 es"
+  shows "c2 = c" quickcheck
 
 (* record mp_state = 
   ballot :: "acc \<Rightarrow> bal"
@@ -69,9 +162,11 @@ fun transit::"event list \<Rightarrow> mp_state" where
         else s)" *)
 
 fun propCmds :: "event list \<Rightarrow> cmd option set" where
-  "propCmds [] = {}"
-  |"propCmds ((Propose c)#es) = (insert (Some c) (propCmds es))"
-  |"propCmds (_#es) = (propCmds es)"
+  "propCmds [] = {}" |
+  "propCmds (e#s) = (let pCmds = propCmds s in
+    case e of Propose c \<Rightarrow> if Some c \<notin> pCmds then insert (Some c) pCmds else pCmds |  _ \<Rightarrow> pCmds)"
+
+lemma "\<forall> S es . propCmds es = S" quickcheck oops
 
 definition safe_at where
   "safe_at m1bmsgs nas \<equiv> (let m1bnum = card m1bmsgs in
