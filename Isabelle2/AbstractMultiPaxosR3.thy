dd0401a6a0f1f5ebe91642c3297119878399e31e
Giuliano Losa <giuliano@losa.fr>
2016-08-24 14:45:06 -0400

    Little more work.

--- Isabelle2/AbstractMultiPaxosR3.thy
+++ Isabelle2/AbstractMultiPaxosR3.thy
@@ -1,5 +1,7 @@
+section {* Distributed and executable algorithm *}
+
 theory AbstractMultiPaxosR3
-imports Main "~~/src/HOL/Library/FinFun" "~~/src/HOL/Library/FSet"
+imports Main "~~/src/HOL/Library/FinFun" "~~/src/HOL/Library/FSet" "~~/src/HOL/Library/Mapping"
 begin
 
 unbundle finfun_syntax
@@ -8,22 +10,24 @@ abbreviation(input) flip where "flip f \<equiv> \<lambda> x y . f y x"
 type_synonym bal = nat
 type_synonym inst = nat
 
+subsection {* Local state and transitions *}
+
+datatype 'v inst_status =
+  Decided 'v | Proposed 'v | Free
+
 record ('a, 'v) local_state =
   -- {* The local state of an acceptor. *}
   id :: 'a
   acceptors :: "'a set"
   ballot :: bal
-  decisions :: "inst \<Rightarrow>f 'v option"
-  leader :: bool
+  log :: "inst \<Rightarrow>f 'v inst_status"
   last_voted :: bal
     -- {* Last ballot in which the acceptor voted. *}
   votes :: "inst \<Rightarrow>f 'v option"
   onebs :: "bal \<Rightarrow>f (inst \<Rightarrow>f ('v\<times>bal) option) option"
     -- {* The oneb messages received when the acceptor tries to acquire leadership. *}
-  twobs :: "inst \<Rightarrow>f bal \<Rightarrow>f 'a fset"
+  twobs :: "inst \<Rightarrow>f bal \<Rightarrow>f 'a set"
     -- {* the twob messages received when the acceptor is a leader. *}
-  next_inst :: inst
-    -- {* The next instance to use for proposing a command. *}
 
 datatype ('a,'v) msg =
   Phase1a 'a bal
@@ -36,8 +40,44 @@ datatype ('a,'v) msg =
 datatype ('a,'v) packet =
   Packet 'a 'a  "('a,'v) msg"
 
+locale local_defs =
+  fixes a::'a
+  and as::"'a set"
+  and leader ::"bal \<Rightarrow> 'a"
+  and quorums :: "'a set set"
+begin
+
+definition local_start where "local_start \<equiv>
+  \<lparr>id = a, acceptors = as, ballot = 0, log = K$ Free,
+    last_voted = 0, votes = K$ None,
+    onebs = K$ None, twobs = K$ K$ {}\<rparr>"
+
+definition next_inst where "next_inst s \<equiv>
+  finfun_to_list (log s)"
+  
+definition do_2a where "do_2a s v \<equiv> 
+  let 
+    inst = 0
+  in True"
+  
+definition propose where "propose s v \<equiv> 
+  if leader (ballot s) = id s 
+  then (s, {Fwd v})
+  else (s, {Fwd v})"
+  
+end
+
+subsection {* Global system IOA. *}
+
 record ('a,'v) global_state =
   local_states :: "'a \<Rightarrow> ('a, 'v)local_state"
   network :: "('a, 'v) packet set"
 
+subsection {* Code generation *}
+
+global_interpretation local_defs a as leader quorums for a as leader quorums 
+  defines start = local_defs.local_start .
+
+export_code start in Scala
+  
 end
