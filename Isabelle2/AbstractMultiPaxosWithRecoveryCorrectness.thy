6a954f28e35758ca7807b383dfce4d26350fda1c
Giuliano Losa <giuliano@losa.fr>
2016-10-05 15:38:56 -0400

    Added AbstractMultiPaxosWithRecoveryCorrectness.thy

---
+++ Isabelle2/AbstractMultiPaxosWithRecoveryCorrectness.thy
@@ -0,0 +1,187 @@
+section {* Proof of the agreement property of AbstractMultiPaxosWithRecovery. *}
+
+theory AbstractMultiPaxosWithRecoveryCorrectness
+imports AbstractMultiPaxosWithRecovery "~~/src/HOL/Eisbach/Eisbach_Tools" BallotArrayProperties 
+begin
+
+locale amp_proof = quorums quorums + ampr_ioa quorums for quorums +
+  fixes the_ioa
+  defines "the_ioa \<equiv> ioa"
+  -- {* Here we have to fix the constant @{term the_ioa} in order to fix all type variables.
+  Otherwise, there are problems in Eisbach when matching the same polymorphic constant appearing in several terms. *}
+begin
+
+interpretation IOA .
+
+subsection {* Automation setup *}
+
+lemmas ioa_defs =
+   is_trans_def actions_def trans_def start_def
+   externals_def ioa_def the_ioa_def paxos_asig_def
+
+named_theorems inv_defs
+
+named_theorems invs
+  -- "named theorem for use by the tactics below"
+named_theorems inv_proofs_defs
+  -- "definitions to unfold"
+
+declare propose_def[simp] join_ballot_def[simp] do_vote_def[simp] crash_def[simp]
+  learn_def[simp] Let_def[simp] if_split[split] if_split_asm[split]
+
+method rm_trans_rel_assm = 
+  (match premises in P[thin]:"trans_rel ?x ?y ?z" \<Rightarrow> \<open>-\<close>)
+
+lemma is_trans_simp[simp]:
+  "s \<midarrow>a\<midarrow>the_ioa\<longrightarrow> t = trans_rel s a t"
+  by (simp add:is_trans_def the_ioa_def ioa_def trans_def)
+  
+method my_fail for msg::"char list" = (print_term msg; fail)
+
+method rm_reachable = (match premises in R[thin]:"reachable ?ioa ?s" \<Rightarrow> \<open>-\<close>)
+
+lemma reach_and_inv_imp_p:"\<lbrakk>reachable ioa s; invariant ioa i\<rbrakk> \<Longrightarrow> i s"
+  by (auto simp add:invariant_def)
+
+method instantiate_invs declares invs =
+  (match premises in I[thin]:"invariant ?ioa ?inv" and R:"reachable ?ioa ?s" \<Rightarrow> \<open>
+    print_fact I, insert reach_and_inv_imp_p[OF R I]\<close>)+
+
+method instantiate_invs_2 declares invs = (
+  (* Deduce that all invariants hold in s *)
+  ( insert invs,
+    instantiate_invs )?,
+  (* Deduce that all invariants hold in t (first deduce that t is reachable) *)
+  match premises in R[thin]:"reachable ?ioa ?s" and T:"?s \<midarrow>?a\<midarrow>?ioa\<longrightarrow> ?t" \<Rightarrow> 
+    \<open>insert reachable_n[OF R T]\<close>,
+  ( insert invs,
+    instantiate_invs )?,
+  (* Get rid of the reachability assumption *)
+  rm_reachable )
+
+text {* If any of m1, m2, or m3 fail, then the whole method fails. *}
+method inv_cases methods m1 m2 m3 uses invs declares inv_defs =
+  (rule invariantI; (
+      ((match premises in "?s \<in> ioa.start ?ioa" \<Rightarrow> \<open>-\<close>); m1 )
+    | ((instantiate_invs_2 invs:invs | my_fail "''instantiation failed''");
+        (m2 | my_fail "''method 2 failed''"); simp only:is_trans_simp;
+          ((induct_tac rule:trans_cases | my_fail "''case analysis failed''"), simp?); rm_trans_rel_assm; m3)
+        ) )
+
+method force_inv uses invs declares inv_defs =
+  inv_cases \<open>(force simp add:inv_defs ioa_defs)?\<close> \<open>-\<close> \<open>(force simp add:inv_defs split:option.splits)?\<close>
+  invs:invs inv_defs:inv_defs
+
+method simp_inv uses invs declares inv_defs =
+  inv_cases \<open>simp add:inv_defs ioa_defs\<close> \<open>-\<close> \<open>(simp add:inv_defs split:option.splits; fail)?\<close>
+  invs:invs inv_defs:inv_defs
+
+method auto_inv uses invs declares inv_defs =
+  inv_cases \<open>auto simp add:inv_defs ioa_defs\<close> \<open>-\<close> \<open>(auto simp add:inv_defs split:option.splits; fail)?\<close>
+  invs:invs inv_defs:inv_defs
+
+subsection {* @{term conservative_array}  is an inductive invariant *}
+
+interpretation ba:ballot_array quorums ballot vote for ballot vote .
+
+definition conservative_array where conservative_array_def[inv_defs]:
+  "conservative_array s \<equiv>  \<forall> i . conservative_at s i"
+
+lemma conservative_inductive:
+  "invariant the_ioa conservative_array"
+  by (force_inv invs: inv_defs:ballot_array.conservative_array_def ballot_array.conservative_def)
+
+subsection {* @{term safe} is invariant } *}
+
+abbreviation safe_at where "safe_at s i \<equiv> ba.safe_at (ballot s) (ba_vote s i)"
+  
+lemma trans_imp_prefix_order:
+  assumes "s \<midarrow>a\<midarrow>the_ioa\<longrightarrow> t"
+  shows "is_prefix_2 quorums (ballot s) (ballot t) (ba_vote s i) (ba_vote t i)" using assms[simplified]
+  apply (cases rule:trans_cases)
+      apply (auto simp add:is_prefix_2_def inv_proofs_defs split:option.split_asm)[3] defer
+   apply (auto simp add:is_prefix_2_def inv_proofs_defs split:option.split_asm)[1]
+  subgoal premises prems for acc
+  proof (simp add:is_prefix_2_def, rule allI)
+    fix a
+    have "(ballot s a \<le> ballot t a) 
+      \<and> (\<forall> b . (b < ballot s a \<or> (b = ballot s a \<and> vote s a b i \<noteq> None)) \<longrightarrow> vote s a b i = vote t a b i)" (is "?P")
+      if "a \<noteq> acc" using prems that by auto
+    moreover
+    { assume "a = acc"
+      have 1:"\<forall>b. vote t a b i = None" using \<open>a = acc\<close> prems by auto
+      have 2:"\<exists> q\<in>quorums . \<forall>a2\<in>q. ballot s a2 \<le> ballot t a"
+      proof -
+        from prems \<open>a = acc\<close> obtain q b where "q \<in> quorums" and "b = Max {ballot s a | a . a \<in> q}"
+            and "ballot t a = b" by (cases s, cases t, auto)
+        moreover have "ballot s a \<le> Max {ballot s a | a . a \<in> q}" (is "ballot s a \<le> Max ?S") if "a \<in> q" for a
+        proof -
+          have "?S \<noteq> {}" and "finite ?S" using \<open>q \<in> quorums\<close> quorums_axioms
+            by (auto simp add:quorums_def)
+          thus ?thesis using that by (metis (mono_tags, lifting) Max.coboundedI mem_Collect_eq) 
+        qed
+        ultimately show ?thesis by auto
+      qed 
+      note 1 2 }
+    ultimately show "ballot s a \<le> ballot t a \<and> (\<forall>b. (b < ballot s a \<longrightarrow> vote s a b i = vote t a b i) \<and> (b = ballot s a \<and> (\<exists>y. vote s a b i = Some y) \<longrightarrow> vote s a (ballot s a) i = vote t a (ballot s a) i)) \<or> (\<exists>q\<in>quorums. \<forall>a2\<in>q. ballot s a2 \<le> ballot t a) \<and> (\<forall>b. vote t a b i = None)"
+      by force 
+  qed
+  done
+
+lemma safe_mono:
+  -- {* @{term safe_at} is monotonic. *}
+  assumes "s \<midarrow>a\<midarrow>the_ioa\<longrightarrow> t" and "safe_at s i v b"
+  shows "safe_at t i v b" using assms ballot_array_prefix.safe_at_mono oops
+by (metis ballot_array_prefix_axioms.intro ballot_array_prefix_def quorums_axioms trans_imp_prefix_order)
+
+abbreviation safe where "safe s \<equiv> \<forall> i . ballot_array.safe  quorums (ballot s) (vote s i)"
+
+lemma safe_votes:
+  assumes "s \<midarrow>aa\<midarrow>the_ioa\<longrightarrow> t" and "vote s i a b  \<noteq> vote t i a b" and "vote t i a b = Some v"
+    and "conservative_array s" and "safe s"
+  shows "safe_at t i v b" 
+  using assms
+  apply (cases aa)
+    apply (auto simp add:inv_proofs_defs)
+  subgoal premises prems
+  proof -
+    have "safe_at s i v (ballot s a)" by (smt assms(4) ballot_array.safe_def ballot_array_props.intro ballot_array_props.proved_safe_at_abs_imp_safe_at option.case_eq_if option.distinct(1) option.sel prems(2) prems(6) quorums_axioms) 
+    thus ?thesis by (metis state.select_convs(2) state.select_convs(3) state.surjective state.update_convs(3) assms(1) fun_upd_apply prems(9) safe_mono) 
+  qed
+  done
+
+lemma safe_inv:
+  "invariant the_ioa safe"
+apply (try_solve_inv2 case_thm:trans_cases inv_proofs_defs:inv_proofs_defs ballot_array.safe_def invs:invs)
+subgoal premises prems for s t act
+proof (auto simp add:ballot_array.safe_def split:option.splits)
+  fix i b a v
+  assume "vote t i a b = Some v"
+  show "safe_at t i v b"
+  proof (cases "vote s i a b = vote t i a b")
+    case True
+    hence "safe_at s i v b" using prems(1) by (metis \<open>vote t i a b = Some v\<close> ballot_array.safe_def option.simps(5))
+    thus ?thesis using prems(2) safe_mono
+      by fastforce 
+  next
+    case False thus ?thesis using safe_votes
+      using \<open>vote t i a b = Some v\<close> prems(1-3) by blast  
+  qed
+qed
+done
+declare safe_inv[invs]
+
+subsection {* Finally, the proof that agreement holds (trivial, follows immediately from safe).*}
+
+definition agreement where 
+  "agreement s \<equiv> \<forall> v w i . chosen s i v \<and> chosen s i w \<longrightarrow> v = w"
+
+lemma agreement:"invariant the_ioa agreement"
+apply(rule invariantI)
+    apply(auto simp add: inv_proofs_defs agreement_def ballot_array.chosen_def ballot_array.chosen_at_def)[1]
+  apply (metis (mono_tags, lifting) IOA.invariant_def IOA.reachable_n agreement_def proof.safe_inv proof_axioms ballot_array_props.intro ballot_array_props.safe_imp_agreement quorums_axioms the_ioa_def)
+done
+
+end
+
+end
